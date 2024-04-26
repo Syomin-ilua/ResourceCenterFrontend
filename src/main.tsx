b90bc43127021e83b3550cad17f3a85b7c1de4dd
Илья Сёмин <semin574@yandex.ru>
2024-04-26 15:22:12 +0300

    Инициализация проекта

---
+++ src/main.tsx
@@ -0,0 +1,76 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
+import "./index.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { Main } from "./pages/main"
+import { Login } from "./pages/auth/login"
+import { Registration } from "./pages/auth/registration"
+
+const container = document.getElementById("root")
+
+const router = createBrowserRouter([
+    {
+        path: "",
+        element: <Main />
+    },
+    {
+        path: "/auth",
+        element: <>Обёртка контента</>,
+        children: [
+            {
+                path: "sign-in",
+                element: <Login />
+            },
+            {
+                path: "sign-up",
+                element: <Registration />
+            }
+        ]
+    },
+    {
+        path: '/courses',
+        element: <>Обёртка контента</>,
+        children: [
+            {
+                path: "",
+                element: <>Курсы</>
+            },
+            {
+                path: "course/:id",
+                element: <>Курс</>
+            }
+        ]
+    },
+    {
+        path: '/profile',
+        element: <>Личный кабинет пользователя</>,
+    },
+    {
+        path: '/admin',
+        element: <>Проверка пользователя на права доступа к админ панели</>,
+        children: [
+            {
+                path: "create-course",
+                element: <>Создать курс</>
+            }
+        ]
+    }
+]);
+
+if (container) {
+    const root = createRoot(container)
+
+    root.render(
+        <React.StrictMode>
+            <Provider store={store}>
+                <RouterProvider router={router} /> 
+            </Provider>
+        </React.StrictMode>,
+    )
+} else {
+    throw new Error(
+        "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+    )
+}
